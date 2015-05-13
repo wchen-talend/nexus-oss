9381fa1a05baa58d6d4873d31d1105153f890dc5
Stuart McCulloch <mcculls@gmail.com>
2015-05-13 13:38:07 +0100

    Fix merge conflict in MetadataServiceIT

--- plugins/npm/nexus-npm-repository-plugin/src/test/java/com/bolyuba/nexus/plugin/npm/service/MetadataServiceIT.java
+++ plugins/npm/nexus-npm-repository-plugin/src/test/java/com/bolyuba/nexus/plugin/npm/service/MetadataServiceIT.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import org.sonatype.nexus.apachehttpclient.Hc4Provider;
 import org.sonatype.nexus.configuration.application.ApplicationDirectories;
 import org.sonatype.nexus.proxy.ResourceStoreRequest;
+import org.sonatype.nexus.proxy.cache.PathCache;
 import org.sonatype.nexus.proxy.item.ContentLocator;
 import org.sonatype.nexus.proxy.item.PreparedContentLocator;
 import org.sonatype.nexus.proxy.item.RepositoryItemUid;
@@ -197,6 +198,9 @@ public class MetadataServiceIT
 
       @Override
       public RepositoryItemUid createUid(String path) { return uid; }
+
+      @Override
+      public PathCache getNotFoundCache() { return mock(PathCache.class); }
     };
 
     // not using mock as it would OOM when it tracks invocations, as we work with large files here
